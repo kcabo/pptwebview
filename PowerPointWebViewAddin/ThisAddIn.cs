e3d882b313ed365846bdd0779e18c709f8290e5e
kcabo <37576764+kcabo@users.noreply.github.com>
2026-09-04 02:44:13 +0900

    Add files via upload

---
+++ PowerPointWebViewAddin/ThisAddIn.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PowerPointWebViewAddin
+{
+    public partial class ThisAddIn
+    {
+        private OverlayWindow overlayWindow;
+
+        private void ThisAddIn_Startup(
+            object sender,
+            EventArgs e
+        )
+        {
+            overlayWindow =
+                new OverlayWindow(
+                    this.Application
+                );
+
+            overlayWindow.Show();
+        }
+
+        private void ThisAddIn_Shutdown(
+            object sender,
+            EventArgs e
+        )
+        {
+            overlayWindow?.Close();
+        }
+
+        #region VSTO で生成されたコード
+
+        private void InternalStartup()
+        {
+            this.Startup +=
+                new EventHandler(
+                    ThisAddIn_Startup
+                );
+
+            this.Shutdown +=
+                new EventHandler(
+                    ThisAddIn_Shutdown
+                );
+        }
+
+        #endregion
+    }
+}
---
+++ PowerPointWebViewAddin/WindowBridge.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PowerPointWebViewAddin
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class WindowBridge
+    {
+        private readonly Window window;
+
+        public WindowBridge(Window window)
+        {
+            this.window = window;
+        }
+
+        public void SetBounds(
+            double screenX,
+            double screenY,
+            double width,
+            double height
+        )
+        {
+            RunOnUiThread(() =>
+            {
+                DpiScale dpi =
+                    VisualTreeHelper.GetDpi(window);
+
+                // PowerPointのscreen pixel
+                // → WPFのDIPへ変換
+                window.Left =
+                    screenX / dpi.DpiScaleX;
+
+                window.Top =
+                    screenY / dpi.DpiScaleY;
+
+                window.Width =
+                    Math.Max(1, width);
+
+                window.Height =
+                    Math.Max(1, height);
+            });
+        }
+
+        public void MoveOffscreen()
+        {
+            RunOnUiThread(() =>
+            {
+                // Window自体は生かしておく。
+                // これによりWebView側のsetIntervalも止まらない。
+                window.Left = -10000;
+                window.Top = -10000;
+            });
+        }
+
+        private void RunOnUiThread(Action action)
+        {
+            if (window.Dispatcher.CheckAccess())
+            {
+                action();
+            }
+            else
+            {
+                window.Dispatcher.Invoke(action);
+            }
+        }
+    }
+}
